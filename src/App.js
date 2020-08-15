159680b8c0e37057133cdac55998456692b58b6d
Pabnan <67246411+Pabnan@users.noreply.github.com>
2020-08-15 17:11:24 +0800

    Tutorial 28-29 async action n redux thunk get request

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import './App.css';
 import CakeContainer from './components/CakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
+
 function App() {
   return (
     <Provider store = {store}>
       <div className="App">
+        <UserContainer/>
+        {/* <ItemContainer cake />
+        <ItemContainer/>
       <HooksCakeContainer/>
       <CakeContainer/>
       <IceCreamContainer/>
-    <NewCakeContainer/>
+    <NewCakeContainer/> */}
     </div>
     </Provider>
   );
